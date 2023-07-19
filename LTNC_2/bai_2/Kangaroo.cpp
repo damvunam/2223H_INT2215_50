c1d965ab3ee76d7e630bf040c80597176550c03f
damvunam <namdv1611@gmail.com>
2023-07-19 21:07:59 +0700

    Done LTNC_1 and bai2

---
+++ LTNC_2/bai_2/Kangaroo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int x1,v1,x2,v2;
+  cin >> x1 >> v1 >> x2 >> v2;
+  if(x1<x2 && v1 > v2){
+    while(x1<x2){
+      x1 += v1;
+      x2 += v2;
+    }
+
+    if(x1==x2){
+    cout << "YES";
+    }
+  }
+  else {cout << "NO";}
+
+}
