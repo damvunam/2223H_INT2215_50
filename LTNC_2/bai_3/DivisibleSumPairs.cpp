468d426d821912a337bd76dea60dd7a49314bb26
damvunam <namdv1611@gmail.com>
2023-07-20 08:10:40 +0700

    done

---
+++ LTNC_2/bai_3/DivisibleSumPairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n,k;
+  cin >> n >> k;
+  int arr[100];
+  int count = 0;
+  for(int i=0;i<n;i++){
+    cin >> arr[i];
+  }
+  for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+      if((arr[i]+arr[j])%k==0){
+        count++;
+      }
+    }
+  }
+  cout << count;
+  return 0;
+}
